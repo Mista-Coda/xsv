df58cbaab844dc6d1bb29f668a6713a10ab545fa
Sebastian2852 <sebastian2852@proton.me>
2026-09-20 20:27:11 +0100

    Better format the pretty print

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 typedef struct {
     Nob_String_View values[256][256];
     size_t rowCount, colCount;
+    size_t rowLen;
 } Csv;
 
 char num_to_char(size_t num) {
@@ -58,25 +59,44 @@ int main(int argc, char** argv) {
         for (size_t col = 0; col < valueCount; ++col) {
             Nob_String_View value = nob_sv_chop_by_delim(&line, ',');
             data.values[row][col] = value;
+            if (value.count > data.rowLen) {
+                data.rowLen = value.count;
+            }
         }
     }
 
-    printf("%zu x %zu\n", data.rowCount, data.colCount);
+    printf("        ");
 
-    printf("       ");
+    size_t spaces = 0;
+    if (data.rowLen % 2 == 0) {
+        spaces = data.rowLen / 2;
+    } else {
+        spaces = (data.rowLen + 1) / 2;
+    }
+
+    Nob_String_Builder spacesStrBuilder = {0};
+    for (size_t i = 0; i < spaces; ++i) {
+        nob_sb_append_cstr(&spacesStrBuilder, " ");
+    }
+    Nob_String_View spacesStr = nob_sb_to_sv(spacesStrBuilder);
 
     for (size_t col = 0; col < data.colCount; ++col) {
-        printf("[  %c  ] ", num_to_char(col));
+        printf("[%.*s%c%.*s] ", SV_Arg(spacesStr), num_to_char(col), SV_Arg(spacesStr));
     }
     printf("\n");
 
     for (size_t col = 0; col < data.colCount; ++col) {
-            printf("[  %c  ] ", num_to_char(col));
-            for (size_t row = 0; row < data.rowCount; ++row) {
-                Nob_String_View value = data.values[col][row];
-                if (value.count == 0) continue;
-                printf("%.*s ", (int)value.count, value.data);
-            }
+        printf("[  %c  ] ", num_to_char(col));
+        for (size_t row = 0; row < data.rowCount; ++row) {
+            Nob_String_View value = data.values[col][row];
+            if (value.count == 0) continue;
+            Nob_String_Builder padding = {0};
+            // +3 to account for the [ X ] header text
+            // +1 to account for the padding the top left of the table
+            for (size_t i = 0; i < (spaces * 2) + 4 - value.count; ++i) nob_sb_append_cstr(&padding, " ");
+            Nob_String_View paddingSv = sb_to_sv(padding);
+            printf("%.*s%.*s", (int)value.count, value.data, SV_Arg(paddingSv));
+        }
         printf("\n");
     }
 
